53fe7a9c4796d188174834f2e79b4609cf93968c
Felipe Marinho <felipmarinho@gmail.com>
2020-07-25 10:30:40 -0300

    calling service

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,28 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FcGoogle } from 'react-icons/fc';
 import { FaFacebook } from 'react-icons/fa';
-import { uuid } from 'uuidv4';
 
 import history from '../../services/history';
 import { Container, Header } from './styles';
 import {Input, Button, ButtonAccess} from '../../components/form';
+import api from '../../services/auth';
 
 function Signin() {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 
-	const handleAccess = () => {
-		sessionStorage.setItem('token', uuid());
-		history.go('/collaborators');
+
+	const handleAccess = async () => {
+		console.log('login', username, password);
+		try {
+			await api.post('/auth', {
+				email: username,
+				password
+			})
+			.then(res => {
+				const { token } = res.data.data;
+				sessionStorage.setItem('token', token);
+				history.go('/collaborators');
+				setPassword('');
+				setUsername('');
+			})
+		} catch (error) {
+			alert('Usuário ou senha estão errados')
+		}
 	}
 
 	return (
 		<Container>
-
 			<Header>
 				<big>Olá novamente</big>
 			</Header>
 
-			<Input placeholder="Usuário:" />
-			<Input placeholder="Senha:" type="password" />
+			<Input
+				placeholder="Usuário:"
+				value={username}
+				onChange={(e) => setUsername(e.target.value)}
+				/>
+			<Input
+				placeholder="Senha:"
+				type="password"
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+			/>
 
 			<Button onClick={() => handleAccess()}>Entrar</Button>
 			<big>OU</big>
