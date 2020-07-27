6cd3d6ba4184ad81c276a2600f6ae9fee02431bc
Felipe Marinho <felipmarinho@gmail.com>
2020-07-27 19:17:11 -0300

    Adicionando middleware

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,36 @@
 import axios from 'axios';
+import history from './history';
 
 const api = axios.create({
   baseURL: 'http://localhost:8080',
 });
+
+api.interceptors.request.use(async (request) => {
+  const token = sessionStorage.getItem('token');
+  if (token) {
+    request.headers.Authorization = `Bearer ${token}`;
+  }
+  return request;
+},
+(error) => (
+  Promise.reject(error)
+));
+
+api.interceptors.response.use(
+  (response) => {
+    const { errors } = response.data;
+    if (!!errors && !!(errors.find((error) => error.message === 'Not authorized'))) {
+      sessionStorage.removeItem('token');
+      sessionStorage.removeItem('user');
+      history.go('/');
+      return Promise.reject(Error());
+    }
+    return response;
+  },
+  (error) => (
+    Promise.reject(error)
+  ),
+);
+
 
 export default api;
