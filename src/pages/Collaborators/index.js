42bd8e861d68e557dfd3432a3ae6b4f87824bd59
Felipe Marinho <felipmarinho@gmail.com>
2020-07-27 19:18:20 -0300

    Buscando colaboradores

--- src/pages/Collaborators/index.js
+++ src/pages/Collaborators/index.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../../components/Card';
 
 import { Container, List, ButtonMore } from './styles';
 
 import Filter from './Filter';
+import { fetchCollaborators } from '../../services/collaborators';
 
 function Collaborators() {
+	const [collaborators, setCollaborators] = useState([]);
+
+	const init = async () => {
+		await fetchCollaborators()
+		.then((res) => {
+			setCollaborators(res.data.content);
+		})
+		.catch(err => {
+			console.log('Error', err);
+		})
+
+	}
+	useEffect(() => {
+		init();
+	}, []);
+
 	return (
 		<Container>
 			<Filter />
 			<List>
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
+				{collaborators && collaborators.map(collaborator => (
+					<Card
+						key={`collaborator${collaborator.id}`}
+						collaborator={collaborator}
+					/>
+				))}
 			</List>
 			<ButtonMore>mais</ButtonMore>
 		</Container>
