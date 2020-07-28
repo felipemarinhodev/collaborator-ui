dff0531dff6f5a2a4013c9560ae36750fca97c7b
Felipe Marinho <felipmarinho@gmail.com>
2020-07-27 21:03:23 -0300

    Adicionando list

--- src/components/form/List/index.js
+++ src/components/form/List/index.js
@@ -1,24 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaPlusCircle } from 'react-icons/fa';
 
 import { Container, InputContainer, ExperiencesContainer } from './styles';
 import { TextArea } from '..';
 import Skills from '../Skills';
 
-function List() {
 
-	const [experiences, setExperiences] = useState([{experience: '', skills: []}]);
+function List({ handleChange }) {
+	const [experiences, setExperiences] = useState([{description: '', skills: []}]);
 
 	const handleAddExperience = () => {
 		setExperiences(experiences => [...experiences, {experience: '', skills: []}])
 	}
+
+	const handleDescription = (value, i) => {
+		const experience = { ...experiences[i], description: value };
+		setExperiences(experiences => {
+			const aux = experiences;
+			aux[i] = experience;
+			return aux;
+		})
+	}
+
+	useEffect(() => {
+		handleChange({ experiences });
+	}, [experiences]);
 
 	return (
 		<Container>
 			<InputContainer>
 				{experiences.map((experience, i) => (
 					<ExperiencesContainer>
-						<TextArea key={`experiencie-${i}`} label="Experiência Profissionais:" />
+						<TextArea
+							key={`experience-${i}`}
+							label="Experiência Profissionais:"
+							value={experience.description}
+							onChange={(e) => handleDescription(e.target.value, i)}
+						/>
 						<Skills label="Compentências" />
 					</ExperiencesContainer>
 				))}
