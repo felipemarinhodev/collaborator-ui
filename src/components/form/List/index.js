b7fb4ca130520e080634695c973eff9cfd2596e1
Felipe Marinho <felipmarinho@gmail.com>
2020-07-30 16:47:35 -0300

    deixando o componente generico

--- src/components/form/List/index.js
+++ src/components/form/List/index.js
@@ -1,47 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { FaPlusCircle } from 'react-icons/fa';
 
 import { Container, InputContainer, ExperiencesContainer } from './styles';
-import { TextArea } from '..';
-import Skills from '../Skills';
 
-
-function List({ handleChange }) {
-	const [experiences, setExperiences] = useState([{description: '', skills: []}]);
-
-	const handleAddExperience = () => {
-		setExperiences(experiences => [...experiences, {experience: '', skills: []}])
-	}
-
-	const handleDescription = (value, i) => {
-		const experience = { ...experiences[i], description: value };
-		setExperiences(experiences => {
-			const aux = experiences;
-			aux[i] = experience;
-			return aux;
-		})
-	}
-
-	useEffect(() => {
-		handleChange({ experiences });
-	}, [experiences]);
+function List(props) {
+	const { children, handleAddMore } = props;
 
 	return (
 		<Container>
 			<InputContainer>
-				{experiences.map((experience, i) => (
-					<ExperiencesContainer>
-						<TextArea
-							key={`experience-${i}`}
-							label="Experiência Profissionais:"
-							value={experience.description}
-							onChange={(e) => handleDescription(e.target.value, i)}
-						/>
-						<Skills label="Compentências" />
-					</ExperiencesContainer>
-				))}
+				{children}
 			</InputContainer>
-			<FaPlusCircle size={30} onClick={() => handleAddExperience()} />
+			<FaPlusCircle size={30} onClick={() => handleAddMore()} />
 		</Container>
 	);
 }
