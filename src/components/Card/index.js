0b7a529945b5bbcb5b30e5735d884920464ab967
Felipe Marinho <felipmarinho@gmail.com>
2020-07-31 08:30:37 -0300

    Usando componente InfoCollaborator

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,28 +1,15 @@
 import React from 'react';
 
-import { TiLightbulb } from 'react-icons/ti';
-
-import { Container, Info } from './styles';
+import { Container } from './styles';
 import Avatar from '../Avatar';
-import history from '../../services/history';
+import InfoCollaborator from '../InfoCollaborator';
 
 function Card({collaborator}) {
 
-	const handleDetailCollaborator = () => {
-		history.go(`/collaborator/${collaborator.id}`);
-	};
-
 	return (
-		<Container onClick={handleDetailCollaborator}>
+		<Container>
 			<Avatar  />
-			<Info>
-				<strong>{collaborator.name}</strong>
-				<small>{collaborator.role}</small>
-				<div>
-					<TiLightbulb size={20} />
-					<small>{collaborator.team}</small>
-				</div>
-			</Info>
+			<InfoCollaborator collaborator={collaborator} />
 		</Container>
 	);
 }
